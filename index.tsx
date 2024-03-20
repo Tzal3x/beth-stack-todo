4fe9d290be1eb10e7dbdf70237d1913f6660a5f3
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-03-20 12:42:59 +0200

    Add tailwind script tag

--- index.tsx
+++ index.tsx
@@ -28,6 +28,7 @@ const BaseHtml = ({ children }: any) => `
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>The BETH stack</title>
       <script src="https://unpkg.com/htmx.org@1.9.3"></script>
+      <script src="https://cdn.tailwindcss.com"></script>
     </head>
     ${children}
   `;
