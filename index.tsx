fcdcb3c19726f1bc5e0ac3091e5b949241b1ef8f
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-03-20 11:36:04 +0200

    Add typed-html
    Using ```
    const BaseHtml = ({children}: elements.Children) => ...
    ```
    resulted in type errors due to incompatibilities between
    `typed-html` and `elysiajs/html`.
    
    To bypass this I was forced to use the `any` keyword.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,23 @@
 import { Elysia } from "elysia";
 import { html } from "@elysiajs/html";
+import * as elements from "typed-html";
 
 const app = new Elysia()
   .use(html())
-  .get("/", ({ html }) => html(baseHtml))
+  .get("/", ({ html }) =>
+    html(
+      <BaseHtml>
+        <h1>Hello, world!</h1>
+      </BaseHtml>,
+    ),
+  )
   .listen(3000);
 
 console.log(
   `🦊 Server is running on http://${app.server?.hostname}:${app.server?.port}`,
 );
 
-const baseHtml = `
+const BaseHtml = ({ children }: any) => `
   <!DOCTYPE html>
   <html lang="en">
     <head>
@@ -18,8 +25,5 @@ const baseHtml = `
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>The BETH stack</title>
     </head>
-    <body>
-      That's the BETH stack!
-    </body>
-  </html>
+    ${children}
   `;
