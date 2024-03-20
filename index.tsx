dc7db67915804e45dc91ba528bd15b0149a12cce
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-03-20 13:05:19 +0200

    Create /todo endpoint

--- index.tsx
+++ index.tsx
@@ -17,6 +17,7 @@ const app = new Elysia()
       </body>
     </BaseHtml>
   ))
+  .get("/todo", () => <TodoList todos={db} />)
   .post("/clicked", () => (
     <div class="flex h-screen w-full items-center justify-center bg-white text-blue-500">
       I'm from the server!
@@ -34,9 +35,41 @@ const BaseHtml = ({ children }: any) => `
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>The BETH stack</title>
+      <title>😺BETH TODO-APP</title>
       <script src="https://unpkg.com/htmx.org@1.9.3"></script>
       <script src="https://cdn.tailwindcss.com"></script>
     </head>
     ${children}
   `;
+
+const TodoItem = ({ content, completed, id }: Todo) => {
+  return (
+    <div class="flex flex-row space-x-3">
+      <p>{content}</p>
+      <input type="checkbox" checked={completed} />
+      <button class={"text-red-500"}>X</button>
+    </div>
+  );
+};
+
+const TodoList = ({ todos }: { todos: Todo[] }) => {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <TodoItem {...todo} />
+      ))}
+    </div>
+  );
+};
+
+type Todo = {
+  id: number;
+  content: string;
+  completed: boolean;
+};
+
+let db: Todo[] = [
+  { id: 1, content: "Learn about Bun", completed: false },
+  // { id: 2, content: "Learn HTMX", completed: false },
+  // { id: 3, content: "Integrate ElysiaJS", completed: false },
+];
