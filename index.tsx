41f48fbf1b68f00c2c8889371b72c6a83aec4ccc
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-03-20 12:28:27 +0200

    Add HTMX script tag

--- index.tsx
+++ index.tsx
@@ -24,6 +24,7 @@ const BaseHtml = ({ children }: any) => `
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>The BETH stack</title>
+      <script src="https://unpkg.com/htmx.org@1.9.3></script>
     </head>
     ${children}
   `;
