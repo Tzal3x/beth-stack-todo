12984893a392f6f207cda1a5b00bf236089e9d18
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-03-20 11:12:16 +0200

    Use html plugin to serve HTML

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
 
-const app = new Elysia().get("/", () => "Hello world!").listen(3000);
+const app = new Elysia()
+  .use(html())
+  .get("/", ({ html }) => html(baseHtml))
+  .listen(3000);
 
 console.log(
   `🦊 Server is running on http://${app.server?.hostname}:${app.server?.port}`,
 );
+
+const baseHtml = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>The BETH stack</title>
+    </head>
+    <body>
+      That's the BETH stack!
+    </body>
+  </html>
+  `;
